18b35b5df06e474636b3b68827c375bca0228225
Tarak311 <tarak_1@live.com>
2018-09-19 18:49:27 -0400

    ds

--- tensor_main.py
+++ tensor_main.py
@@ -1,2 +1,5 @@
-
+#!/usr/bin/python
 import tensorflow as tf
+model =tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(512.activation=tf.nn.relu, input_shape=(784,)))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
